06c4a72d8061835a2d8f4523249d8e7b1b1e2c1e
Eduardo Bezerra <edubezerra@gmail.com>
2015-11-08 19:03:44 -0200

    Documentos de especificação adicionados.

---
+++ test/br/cefetrj/sca/servico/FornecerGradeDisponibilidadeServiceTest.java
@@ -0,0 +1,33 @@
+package br.cefetrj.sca.servico;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.cefetrj.sca.dominio.gradesdisponibilidade.FichaDisponibilidade;
+import br.cefetrj.sca.service.FornecerGradeDisponibilidadeService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+@TransactionConfiguration
+@Transactional
+public class FornecerGradeDisponibilidadeServiceTest extends
+		AbstractTransactionalJUnit4SpringContextTests {
+
+	@Autowired
+	protected FornecerGradeDisponibilidadeService servico;
+
+	@Test
+	public void testValidarMatricula() {
+		assertNotNull("Serviço não iniciado.", servico);
+		FichaDisponibilidade ficha = servico.validarProfessor("1506449");
+		assertEquals(ficha.getMatriculaProfessor(), "1506449");
+	}
+}
