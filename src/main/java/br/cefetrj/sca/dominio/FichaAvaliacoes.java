4a86584cf6400039d2d72f8ff0f07d4d57abcb39
Eduardo Bezerra <edubezerra@gmail.com>
2016-04-04 21:27:50 -0300

    Iniciando implementação de Registrar Avaliação

---
+++ src/main/java/br/cefetrj/sca/dominio/FichaAvaliacoes.java
@@ -0,0 +1,46 @@
+package br.cefetrj.sca.dominio;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FichaAvaliacoes {
+	String nomeProfessor;
+	String matriculaProfessor;
+	String codigoTurma;
+	String nomeDisciplina;
+
+	class ItemFicha {
+		String nomeAluno;
+		String matriculaAluno;
+		BigDecimal notaP1;
+		BigDecimal notaP2;
+		BigDecimal notaP3;
+		Integer qtdFaltas;
+	}
+
+	List<ItemFicha> itensFicha = new ArrayList<>();
+
+	public String getNomeDisciplina() {
+		return nomeDisciplina;
+	}
+
+	public String getNomeProfessor() {
+		return nomeProfessor;
+	}
+
+	public String getMatricularProfessor() {
+		return matriculaProfessor;
+	}
+
+	public String getCodigoTurma() {
+		return codigoTurma;
+	}
+
+	public void lancar(String matriculaAluno, 
+			BigDecimal notaP1, 
+			BigDecimal notaP2, 
+			BigDecimal notaP3,
+			Integer qtdFaltas) {
+	}
+}
---
+++ src/main/java/br/cefetrj/sca/service/RegistrarAvaliacaoService.java
@@ -0,0 +1,23 @@
+package br.cefetrj.sca.service;
+
+import java.util.List;
+
+import br.cefetrj.sca.dominio.FichaAvaliacoes;
+import br.cefetrj.sca.dominio.Turma;
+
+public class RegistrarAvaliacaoService {
+	List<Turma> iniciarLancamentoNotas(String matriculaProfessor) {
+		return null;
+	}
+
+	FichaAvaliacoes solicitarLancamento(String codigoTurma) {
+		return null;
+	}
+
+	void registrarLancamentos(FichaAvaliacoes ficha) {
+
+	}
+
+	void confirmarLancamentoAvaliacoes() {
+	}
+}
