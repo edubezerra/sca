fad5f7d1ffc58d993520e5cb86d18e453700579b
Joao <Joao@192.168.1.4>
2016-09-14 20:43:39 -0300

    Atualização AvaliacaoTurmaRepositorio e TurmaRepositorio

--- src/main/java/br/cefetrj/sca/dominio/repositories/AvaliacaoTurmaRepositorio.java
+++ src/main/java/br/cefetrj/sca/dominio/repositories/AvaliacaoTurmaRepositorio.java
@@ -1,6 +1,7 @@
 package br.cefetrj.sca.dominio.repositories;
 
 import java.io.Serializable;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,4 +13,10 @@ public interface AvaliacaoTurmaRepositorio extends
 
 	@Query("SELECT a FROM AvaliacaoTurma a WHERE a.turmaAvaliada.id = ?1 AND a.alunoAvaliador.matricula = ?2")
 	public AvaliacaoTurma getAvaliacaoTurma(Long idTurma, String matricula);
+	
+	@Query("SELECT a from AvaliacaoTurma a WHERE a.turmaAvaliada.id = ?1")
+	public AvaliacaoTurma findAvaliacoesTurma(Long idTurma);
+	
+	@Query("SELECT a from AvaliacaoTurma a WHERE a.turmaAvaliada.id = ?1")
+	public List<AvaliacaoTurma> findAvaliacoesTurmaLista(Long idTurma);
 }
