38250cbce96a29c86cb7b83db32414dbb63ab8a1
Joao <Joao@192.168.1.3>
2016-09-29 23:47:27 -0300

    Melhoria no metodo conversaoRespospa()

--- src/main/java/br/cefetrj/sca/service/VisualizacaoAvaliacaoDiscenteService.java
+++ src/main/java/br/cefetrj/sca/service/VisualizacaoAvaliacaoDiscenteService.java
@@ -24,6 +24,9 @@ public class VisualizacaoAvaliacaoDiscenteService {
 	@Autowired
 	private AvaliacaoTurmaRepositorio repositorio2;
 
+	private String[] PerguntasAvaliacao = { "Primeira", "Segunda", "Terceira", "Quarta", "Quinta", "Sexta", "Setima",
+			"Oitava" };
+
 	public List<Turma> listarTurmaLecionadas(Professor p) {
 		List<Turma> t = repositorio.findTurmasLecionadasPorProfessor(p.getMatricula());
 		List<Turma> turmasAvaliadas = new ArrayList<>();
@@ -53,7 +56,7 @@ public class VisualizacaoAvaliacaoDiscenteService {
 	public void conversaoRespospa(List<AvaliacaoTurma> avaliacaoTurma) throws IOException {
 		BufferedWriter strW = new BufferedWriter(new FileWriter("C:/Users/Joao/git/sca/tabela.csv"));
 		strW.write(
-				"Pergunta,Insuficiente(s) ou Ruim(ns),Suficiente(s) ou Regular(es),Bom(ns) ou Boa(s),Ótimo(s) ou Ótima(s)\n");
+				"Pergunta,Insuficiente(s) ou Ruim(ns),Suficiente(s) ou Regular(es),Bom(ns) ou Boa(s),Otimo(s) ou Otima(s)\n");
 		List<Alternativa> resp = new ArrayList<>();
 		for (int i = 0; i < avaliacaoTurma.size(); i++) {
 			AvaliacaoTurma a = avaliacaoTurma.get(i);
@@ -85,8 +88,8 @@ public class VisualizacaoAvaliacaoDiscenteService {
 			}
 			posicao += 8;
 			if (posicao >= resp.size()) {
-				strW.write(posicaoPerg + "," + valorRespostas[3] + "," + valorRespostas[2] + "," + valorRespostas[1]
-						+ "," + valorRespostas[0] + "\n");
+				strW.write(PerguntasAvaliacao[posicaoPerg] + "," + valorRespostas[3] + "," + valorRespostas[2] + ","
+						+ valorRespostas[1] + "," + valorRespostas[0] + "\n");
 				posicaoPerg += 1;
 				posicao = posicaoPerg;
 				valorRespostas = new int[4];
